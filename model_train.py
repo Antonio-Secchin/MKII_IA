bf953937f5d34e9bafef71de9426edce483f1e36
Antonio <antonio-lucio-s@hotmail.com>
2025-06-26 10:04:02 -0300

    Treinando por mais tempo e detalhes que eu percebi

--- model_train.py
+++ model_train.py
@@ -30,6 +30,10 @@ def make_env():
     wraper_env = LastActionsWrapper(env, n_actions=10)
     return wraper_env
 
+def make_env_image():
+    env = retro.make(game='MortalKombatII-Genesis', state='Level1.LiuKangVsJax', obs_type=Observations.IMAGE, render_mode = None)
+    return env
+
 def make_info_env(var_names):
     env = retro.make(game='MortalKombatII-Genesis', state='Level1.LiuKangVsJax', obs_type=Observations.RAM, render_mode = None)
     wraper_env = InfoActionHistoryWrapper(env, var_names=var_names, n_actions=10)
@@ -62,7 +66,7 @@ variables = [
 
 #Torna o ambiente vetorizado (requerido por SB3)
 #vec_env = DummyVecEnv([make_env])
-info_env = make_env()
+info_env = make_info_env(variables)
 vec_env = DummyVecEnv([lambda:info_env])
 
 
